f7c2df5f11cbf467098c45ee4f9d6a40b80e4f1a
Hampus Andersson <70724079+Hampster8@users.noreply.github.com>
2022-03-11 23:11:35 +0100

    Update app.js

--- express-mongoose/app.js
+++ express-mongoose/app.js
@@ -9,6 +9,24 @@ connection();
 // middleware
 app.use(express.json()); // lets us work with JSON in backend and frontend
 
+
+app.post('/user', (req, res) => {
+    const data = new User(req-body);
+    data.save()
+    .then(user => {
+        console.log('user saved', user);
+        res.json({
+            success: true,
+            user
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+});
+
+
 app.listen(PORT, () => {
     console.log('App running on port ' + PORT);
 });
